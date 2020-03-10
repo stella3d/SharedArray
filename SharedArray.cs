7611bfd9aec16e365acf2964d564c128a5ed2fa9
Stella Cannefax <stella@unity3d.com>
2020-03-10 02:07:22 -0700

    update

--- SharedArray.cs
+++ SharedArray.cs
@@ -4,34 +4,45 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
 
 namespace Stella3D
 {
-    public class SharedArray<T> : IDisposable, IEnumerable<T>
-        where T : unmanaged
+    public class SharedArray<T> : SharedArray<T, T> where T : unmanaged 
     {
-        GCHandle m_GcHandle;
-        AtomicSafetyHandle m_SafetyHandle;
+        public SharedArray(T[] managed) { Initialize(managed); }
+    }
 
-        T[] m_Managed;
-        NativeArray<T> m_Native;
+    public class SharedArray<T, TNative> : IDisposable, IEnumerable<T> 
+        where T : unmanaged 
+        where TNative : unmanaged
+    {
+        protected GCHandle m_GcHandle;
+        protected AtomicSafetyHandle m_SafetyHandle;
+
+        protected T[] m_Managed;
+        protected NativeArray<TNative> m_Native;
 
         public int Length => m_Managed.Length;
 
-        public SharedArray(T[] managed)
+        protected SharedArray() { }
+
+        public unsafe SharedArray(T[] managed)
         {
-            m_Managed = managed;
+            if (sizeof(T) != sizeof(TNative))
+            {
+                var msg = $"size of native alias type {typeof(TNative).FullName} ({sizeof(TNative)} bytes) " +
+                          $"must be equal to size of type {typeof(T).FullName} ({sizeof(T)} bytes)";
 
-            // Unity's garbage collector doesn't move objects around, so this should not even be necessary.
-            // there's not much downside to playing it safe, though
-            m_GcHandle = GCHandle.Alloc(m_Managed, GCHandleType.Pinned);
-
-            InitializeNative();
+                throw new InvalidOperationException(msg);
+            }
+            
+            Initialize(managed);
         }
 
         ~SharedArray() { Dispose(); }
         
-        public static implicit operator T[](SharedArray<T> self)
+        public static implicit operator T[](SharedArray<T, TNative> self)
         {
 #if UNITY_EDITOR && !DISABLE_SHAREDARRAY_SAFETY
             // CheckWrite also checks for any other readers
@@ -40,17 +51,26 @@ namespace Stella3D
             return self.m_Managed;
         }
 
-        public static implicit operator NativeArray<T>(SharedArray<T> self)
+        public static implicit operator NativeArray<TNative>(SharedArray<T, TNative> self)
         {
             return self.m_Native;
         }
 
-        unsafe void InitializeNative()
+        protected void Initialize(T[] managed)
+        {
+            m_Managed = managed;
+            // Unity's garbage collector doesn't move objects around, so this should not even be necessary.
+            // there's not much downside to playing it safe, though
+            m_GcHandle = GCHandle.Alloc(m_Managed, GCHandleType.Pinned);
+            InitializeNative();
+        }
+
+        protected unsafe void InitializeNative()
         {
             fixed (void* ptr = m_Managed)
             {
-                m_Native = NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray<T>(ptr, m_Managed.Length,
-                    Allocator.None);
+                m_Native = NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray<TNative>
+                    (ptr, m_Managed.Length, Allocator.None);
             }
 
 #if UNITY_EDITOR
@@ -64,10 +84,14 @@ namespace Stella3D
             if (newSize == m_Managed.Length)
                 return;
 
+            // no jobs can be using the data when we resize it
+            AtomicSafetyHandle.CheckWriteAndThrow(m_SafetyHandle);
+
             if (m_GcHandle.IsAllocated) m_GcHandle.Free();
             Array.Resize(ref m_Managed, newSize);
             m_GcHandle = GCHandle.Alloc(m_Managed, GCHandleType.Pinned);
 
+            AtomicSafetyHandle.Release(m_SafetyHandle);
             InitializeNative();
         }
         
